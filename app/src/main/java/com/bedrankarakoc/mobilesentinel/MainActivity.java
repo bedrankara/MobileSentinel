f39c6354796e3696386e1dbe80a23c95a7d2016b
Bedran Karakoc <bedran.karakoc@ruhr-uni-bochum.de>
2020-04-28 16:58:33 +0200

    Add permission handling and setup Python

--- app/src/main/java/com/bedrankarakoc/mobilesentinel/MainActivity.java
+++ app/src/main/java/com/bedrankarakoc/mobilesentinel/MainActivity.java
@@ -4,11 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+
 public class MainActivity extends AppCompatActivity {
+
+    private String[] permissions = {"android.permissions.READ_EXTERNAL_STORAGE", "android.permission.WRITE_EXTERNAL_STORAGE", "android.permissions.READ_PHONE_STATE"};
+    private int requestCode = 1337;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestPermissions(permissions, requestCode);
+
+        if (!Python.isStarted())
+            Python.start(new AndroidPlatform(this));
+
+
     }
 }
