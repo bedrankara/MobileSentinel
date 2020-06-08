69a0ba16a2518c8d64fcfb6a04e265092a3ccd29
Bedran Karakoc <bedran.karakoc@ruhr-uni-bochum.de>
2020-06-08 19:01:56 +0200

    Added onCreate method in DetectionFragment

--- app/src/main/java/com/bedrankarakoc/mobilesentinel/DetectionFragment.java
+++ app/src/main/java/com/bedrankarakoc/mobilesentinel/DetectionFragment.java
@@ -4,13 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class DetectionFragment extends Fragment {
 
+
+
     View view;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     @Nullable
     @Override
