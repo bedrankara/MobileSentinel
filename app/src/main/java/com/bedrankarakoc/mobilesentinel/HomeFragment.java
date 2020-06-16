782eaf688b7add682de063b69b046f2312ce29ef
Bedran Karakoc <bedran.karakoc@rub.de>
2020-06-16 14:00:14 +0200

    HomeFragment shows serving cell information now

--- app/src/main/java/com/bedrankarakoc/mobilesentinel/HomeFragment.java
+++ app/src/main/java/com/bedrankarakoc/mobilesentinel/HomeFragment.java
@@ -1,16 +1,32 @@
 package com.bedrankarakoc.mobilesentinel;
 
+import com.bedrankarakoc.mobilesentinel.BaseStation;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.telephony.CellIdentityGsm;
+import android.telephony.CellIdentityLte;
+import android.telephony.CellIdentityWcdma;
+import android.telephony.CellInfo;
+import android.telephony.CellInfoGsm;
+import android.telephony.CellInfoLte;
+import android.telephony.CellInfoWcdma;
+import android.telephony.TelephonyManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.List;
+
 public class HomeFragment extends Fragment {
 
     View view;
+    private TelephonyManager telephonyManager;
+
 
     @Nullable
     @Override
@@ -18,4 +34,105 @@ public class HomeFragment extends Fragment {
         view =  inflater.inflate(R.layout.home_fragment, container, false);
         return view;
     }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        telephonyManager = (TelephonyManager) getActivity().getSystemService(Context.TELEPHONY_SERVICE);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        showCellinfo(view);
+    }
+
+
+    public void showCellinfo(View view) {
+        TextView cellInfoView = view.findViewById(R.id.cellInfoView);
+        List<CellInfo> cellInfoList = null;
+        try {
+            cellInfoList = telephonyManager.getAllCellInfo();
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+
+
+
+        if (cellInfoList == null) {
+            cellInfoView.setText("Activate your GPS for cellinfo");
+        } else if (cellInfoList.size() == 0) {
+            cellInfoView.setText("Base station list empty");
+        } else {
+            int cellNumber = cellInfoList.size();
+            BaseStation servingBaseStation = bindData(cellInfoList.get(0));
+            cellInfoView.setText("Obtained " + cellNumber + " Base Stations" +  "\nServing Base station：\n" + servingBaseStation.toString());
+            for (CellInfo cellInfo : cellInfoList) {
+                BaseStation bs = bindData(cellInfo);
+                System.out.println(bs.toString());
+
+            }
+        }
+
+    }
+
+    private BaseStation bindData(CellInfo cellInfo) {
+        BaseStation baseStation = null;
+        if (cellInfo instanceof CellInfoWcdma) {
+            //3G
+            CellInfoWcdma cellInfoWcdma = (CellInfoWcdma) cellInfo;
+            CellIdentityWcdma cellIdentityWcdma = cellInfoWcdma.getCellIdentity();
+            baseStation = new BaseStation();
+            baseStation.setType("WCDMA");
+            baseStation.setCid(cellIdentityWcdma.getCid());
+            baseStation.setLac(cellIdentityWcdma.getLac());
+            baseStation.setMcc(cellIdentityWcdma.getMcc());
+            baseStation.setMnc(cellIdentityWcdma.getMnc());
+            baseStation.setBsic_psc_pci(cellIdentityWcdma.getPsc());
+            if (cellInfoWcdma.getCellSignalStrength() != null) {
+                baseStation.setAsuLevel(cellInfoWcdma.getCellSignalStrength().getAsuLevel()); //Get the signal level as an asu value between 0..31, 99 is unknown Asu is calculated based on 3GPP RSRP.
+                baseStation.setSignalLevel(cellInfoWcdma.getCellSignalStrength().getLevel()); //Get signal level as an int from 0..4
+                baseStation.setDbm(cellInfoWcdma.getCellSignalStrength().getDbm()); //Get the signal strength as dBm
+            }
+        } else if (cellInfo instanceof CellInfoLte) {
+            //4G
+            CellInfoLte cellInfoLte = (CellInfoLte) cellInfo;
+            CellIdentityLte cellIdentityLte = cellInfoLte.getCellIdentity();
+
+            baseStation = new BaseStation();
+            baseStation.setType("LTE");
+            baseStation.setCid(cellIdentityLte.getCi());
+            baseStation.setMnc(cellIdentityLte.getMnc());
+            baseStation.setMcc(cellIdentityLte.getMcc());
+            baseStation.setLac(cellIdentityLte.getTac());
+            baseStation.setBsic_psc_pci(cellIdentityLte.getPci());
+
+            if (cellInfoLte.getCellSignalStrength() != null) {
+                baseStation.setAsuLevel(cellInfoLte.getCellSignalStrength().getAsuLevel());
+                baseStation.setSignalLevel(cellInfoLte.getCellSignalStrength().getLevel());
+                baseStation.setDbm(cellInfoLte.getCellSignalStrength().getDbm());
+            }
+        } else if (cellInfo instanceof CellInfoGsm) {
+            //2G
+            CellInfoGsm cellInfoGsm = (CellInfoGsm) cellInfo;
+            CellIdentityGsm cellIdentityGsm = cellInfoGsm.getCellIdentity();
+            baseStation = new BaseStation();
+            baseStation.setType("GSM");
+            baseStation.setCid(cellIdentityGsm.getCid());
+            baseStation.setLac(cellIdentityGsm.getLac());
+            baseStation.setMcc(cellIdentityGsm.getMcc());
+            baseStation.setMnc(cellIdentityGsm.getMnc());
+            baseStation.setBsic_psc_pci(cellIdentityGsm.getPsc());
+            if (cellInfoGsm.getCellSignalStrength() != null) {
+                baseStation.setAsuLevel(cellInfoGsm.getCellSignalStrength().getAsuLevel());
+                baseStation.setSignalLevel(cellInfoGsm.getCellSignalStrength().getLevel());
+                baseStation.setDbm(cellInfoGsm.getCellSignalStrength().getDbm());
+            }
+        } else {
+
+            System.out.println("CDMA CellInfo................................................");
+        }
+        return baseStation;
+    }
+
 }
