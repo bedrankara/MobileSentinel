719ae7a4528768096a58c92fb0152b4a73eb0646
Bedran Karakoc <bedran.karakoc@rub.de>
2020-06-21 14:42:24 +0200

    Modificated detection handler

--- app/src/main/python/statemachine/detection_handler.py
+++ app/src/main/python/statemachine/detection_handler.py
@@ -1,10 +1,38 @@
 from statemachine.detection_states import InitState
-
+from parsers.qualcomm.pycrate.pycrate_asn1dir import RRCLTE
+import string
+from binascii import hexlify, unhexlify
+import json
 
 class Detection(object):
 
     def __init__(self):
         self.state = InitState()
 
+    def check_packet(self, rrc_subtype, msg):
+
+        if str(rrc_subtype) != "gsmtap_lte_rrc_types.DL_DCCH":
+
+            sch = RRCLTE.EUTRA_RRC_Definitions.DL_DCCH_Message
+            sch.from_uper(unhexlify(msg))
+            json_string = sch.to_json()
+            data = json.loads(json_string)
+            if 'rrcConnectionReconfiguration' in data['message']['c1']:
+                return data
+
+
+
+
+
+
+
+
+
     def on_event(self, rrc_subtype, msg):
-        self.state = self.state.on_event(rrc_subtype, msg)
+
+        parsed_msg = self.check_packet(rrc_subtype, msg)
+        if parsed_msg is not None:
+            self.state = self.state.on_event(msg)
+
+
+
