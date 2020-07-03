8bcff743aef5007d36c22704a6d024d1e7816ee8
Bedran Karakoc <bedran.karakoc@ruhr-uni-bochum.de>
2020-07-03 12:37:09 +0200

    Make sure log folder is created

--- app/src/main/python/setup_parser.py
+++ app/src/main/python/setup_parser.py
@@ -19,6 +19,8 @@ import struct
 import util
 import faulthandler
 import logging
+import os.path
+from os import path
 
 
 GSMTAP_PORT = 4729
@@ -26,6 +28,13 @@ IP_OVER_UDP_PORT = 47290
 
 
 def start_logging(filename):
+    # Check if log folder exists and has permissions
+    try:
+        print(subprocess.check_output(['su','-c', 'mkdir /data/media/0/logs']))
+        print(subprocess.check_output(['su','-c', 'chmod 777 /data/media/0/logs']))
+    except Exception as e:
+        print(e)
+    # Initiate mdlog and pass config files
     print(subprocess.check_output(['su','-c', 'mkdir /data/media/0/logs/'+filename]))
     print(subprocess.check_output(['su','-c', 'chmod 777 /data/media/0/logs/'+filename]))
     subprocess.Popen(['su','-c','diag_mdlog -s 90000 -f /data/media/0/logs/RRCDIAG.cfg -o /data/media/0/logs/'+filename])
@@ -38,7 +47,7 @@ def stop_logging():
 
 def initiate_parsing(packet_list,dump_directory,dump_filename, detection_view=None):
 
-    # Hardcoded values
+    # Setup dump parser modules
     my_parser = parsers.QualcommParser(packet_list, detection_view)
     d = str(Environment.getExternalStorageDirectory());
     print(d)
